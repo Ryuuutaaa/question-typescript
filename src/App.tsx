274ec806c1da116dcf6c2746ab30321341313d2e
Ryuuutaaa <rafiakmalwidikta@gmail.com>
2024-05-09 17:43:58 +0800

    next app text dont done

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,16 @@
+import { useState } from "react";
+import QuestionCard from "./components/QuestionCard";
+
+const TOTAL_QUESTION = 10;
+
 const App = () => {
+  const [loading, setLoading] = useState(false);
+  const [questions, setQuestions] = useState([]);
+  const [number, setNumber] = useState(0);
+  const [userAnswers, setUserAnswers] = useState([]);
+  const [score, setScore] = useState(0);
+  const [gameOver, setGameOver] = useState(true);
+
   const startTrivia = async () => {};
 
   const cheackAnswer = (e: React.MouseEvent<HTMLButtonElement>) => {};
@@ -13,6 +25,18 @@ const App = () => {
           Start
         </button>
         <p className="score">Score</p>
+        <p>Looading Question....</p>
+        <QuestionCard
+          questionNr={number + 1}
+          totalQuestion={TOTAL_QUESTION}
+          question={questions[number].question}
+          answers={questions[number].answers}
+          userAnswers={userAnswers ? userAnswers[number] : undefined}
+          callback={cheackAnswer}
+        />
+        <button className="next" onClick={nextQuestion}>
+          Next
+        </button>
       </div>
     </>
   );
--- src/components/QuestionCard.tsx
+++ src/components/QuestionCard.tsx
@@ -1,7 +1,38 @@
 import React from "react";
 
-const QuestionCard = () => {
-  return <div>QuestionCard</div>;
+type Props = {
+  question: string;
+  answers: string[];
+  callback: any;
+  userAnswers: any;
+  quetionNr: number;
+  totalQuestion: number;
+};
+
+const QuestionCard: React.FC<Props> = ({
+  question,
+  answers,
+  callback,
+  userAnswers,
+  quetionNr,
+  totalQuestion,
+}) => {
+  return (
+    <div>
+      <p className="number">
+        Qestion : {question} / {totalQuestion}
+      </p>
+      <div>
+        {answers.map((answers) => (
+          <div>
+            <button disabled={userAnswers} onClick={callback}>
+              <span dangerouslySetInnerHTML={{ __html: answers }}></span>
+            </button>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default QuestionCard;
